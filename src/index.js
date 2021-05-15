a36c4c9fe167042245763c4c80b15308f6986a20
Francisco Veloz <franciscoveloz245@gmail.com>
2021-05-15 12:06:15 -0500

    Movimos public arriba de las routes

--- src/index.js
+++ src/index.js
@@ -49,6 +49,9 @@ app.use((req, res, next) => {
     next()
 })
 
+//Public
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Routes
 app.use(require('./routes/index.route'));
 app.use(require('./routes/auth.route'));
@@ -58,9 +61,6 @@ app.use(require('./routes/admin.dash.route'));
 app.use(require('./routes/admin.admin.route'));
 app.use(require('./routes/admin.super.route'));
 app.use(require('./routes/admin.user.route'));
-
-//Public
-app.use(express.static(path.join(__dirname, 'public')));
 
 //Starting
 app.listen(app.get('port'), () => {
