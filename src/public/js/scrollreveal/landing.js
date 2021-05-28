da4dd1621881db112b813b93d90f3713ea757ee7
Francisco Veloz <franciscoveloz245@gmail.com>
2021-05-28 14:32:04 -0500

    Terminamos el scrollreveal de la landing page

---
+++ src/public/js/scrollreveal/landing.js
@@ -0,0 +1,74 @@
+window.sr = ScrollReveal();
+
+//MAIN
+sr.reveal('#main-text', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#main-image', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+//PRODUCTS
+sr.reveal('#pro-1', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#pro-2', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+
+sr.reveal('#pro-3', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+//SOFTWARE
+sr.reveal('#soft-img', {
+    duration: 1000,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('#soft-text', {
+    duration: 1500,
+    origin: 'top',
+    distance: '150px',
+});
+
+//TARJETAS
+sr.reveal('#tar-1', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#tar-2', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#tar-3', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+//CONTACT
+sr.reveal('#contact', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 500
+});
